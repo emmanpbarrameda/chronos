81ded528abad4e238c9cc6d20b51dad05b97b23a
“ianstudios” <ianstudioscom@gmail.com>
2026-02-02 15:18:11 +0700

    update create_chronos_audits_table.php

--- database/migrations/create_chronos_audits_table.php
+++ database/migrations/create_chronos_audits_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
             
             $table->timestamps();
             
-            // Indexing agar query history cepat
             $table->index(['auditable_type', 'auditable_id']);
         });
     }
